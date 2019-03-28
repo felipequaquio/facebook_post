a1c05e621264d1d1b9fc82fd28b481ebf04fed76
Felipe <felipeferreiradev@gmail.com>
2019-03-28 19:04:39 -0300

    Alterado AppServiceProvider para tentar corrigir erro de carregamento de arquivos css e js em produção.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(env('REDIRECT_HTTPS')) {
+        if(env('APP_ENV') !== 'local') {
             $url->forceSchema('https');
         }
     }
